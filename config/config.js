25a54f06e1649887ae24a7c1b366b80699206f2f
Paolo <bison@technikum-wien.at>
2017-10-04 11:41:52 +0200

    Removed example files

--- config/config.js
+++
@@ -1,7 +0,0 @@
-const DEBUG = true;
-
-const PROTOCOL = 'http';
-const HOST = 'debian.dev';
-const PROJECT = 'ClientAddonTemplate';
-const PATH = 'controller';
-const ROUTER = 'router.php';
--- js/aufnahmeLogin.js
+++
@@ -1,9 +0,0 @@
-//
-$(document).ready(function() {
-
-	checkLogin(checkLoginSuccessLogin);
-
-	//
-	$("#btnLogin").click(callLogin);
-
-});
