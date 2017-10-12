a19b231b58a92af636113a386479b8da86bb515c
Paolo <bison@technikum-wien.at>
2017-10-12 16:40:52 +0200

    Better JS example

--- js/CoreClient.js.ex
+++ js/CoreClient.js.ex
@@ -37,55 +37,68 @@ var loginParameters = {
 	password: "*******"
 };
 
-// Example GET call to login
-// NOTE: The last parameter is not specified because the cache will be automatically managed,
-// no way to change the behaviour
-CoreClient.callRESTFulGet(
-	LOGIN_CALL_NAME,	// Name of the remote call, an alias it will be translated to a call to the core
-	loginParameters,	// Object that contains parameters to send to the remote web service
-	errorCallback,		// Function reference to manage errors
-	successCallback		// Function reference to manage success
-);
+$(document).ready(function() { // needed to use the veil
+	// Example GET call to login
+	// NOTE: The last parameter is not specified because the cache will be automatically managed,
+	// no way to change the behaviour
+	CoreClient.callRESTFulGet(
+		LOGIN_CALL_NAME,	// Name of the remote call, an alias it will be translated to a call to the core
+		loginParameters,	// Object that contains parameters to send to the remote web service
+		{
+			errorCallback: errorCallback,		// Function reference to manage errors
+			successCallback: successCallback,	// Function reference to manage success
+			veilTimeout: 2000					// overwrite veil timeout
+		}
+	);
 
-// Example GET call. AUTH = no, HOOK = yes, CACHE = enabled
-CoreClient.callRESTFulGet(
-	'testHookNoLogin',
-	null,
-	errorCallback,
-	successCallback,
-	CACHE_ENABLED
-);
+	// Example GET call. AUTH = no, HOOK = yes, CACHE = enabled
+	CoreClient.callRESTFulGet(
+		'testHookNoLogin',
+		null,
+		{
+			errorCallback: errorCallback,
+			successCallback: successCallback,
+			cache: CACHE_ENABLED
+		}
+	);
 
-// Example GET call. AUTH = yes, HOOK = no, CACHE = disabled
-CoreClient.callRESTFulGet(
-	'testNoHook',
-	null,
-	errorCallback,
-	successCallback,
-	CACHE_DISABLED
-);
+	// Example GET call. AUTH = yes, HOOK = no, CACHE = disabled
+	CoreClient.callRESTFulGet(
+		'testNoHook',
+		null,
+		{
+			errorCallback: errorCallback,
+			successCallback: successCallback,
+			cache: CACHE_DISABLED
+		}
+	);
 
-// Example GET call. AUTH = yes, HOOK = no, CACHE = overwrite, SESSION_PARAMS = person_id
-// NOTE: If a person_id is specified as parameter, it will be overwritten by the session parameter
-CoreClient.callRESTFulGet(
-	'loadPersonData',
-	null,
-	errorCallback,
-	successCallback,
-	CACHE_OVERWRITE
-);
+	// Example GET call. AUTH = yes, HOOK = no, CACHE = overwrite, SESSION_PARAMS = person_id
+	// NOTE: If a person_id is specified as parameter, it will be overwritten by the session parameter
+	CoreClient.callRESTFulGet(
+		'loadPersonData',
+		null,
+		{
+			errorCallback: errorCallback,
+			successCallback: successCallback,
+			cache: CACHE_OVERWRITE
+		}
+	);
 
-// Object that contains parameters to send to the remote web service
-var saveDataPersonParameters = {
-	name: "me",
-	surname: "alwaysme"
-};
+	// Object that contains parameters to send to the remote web service
+	var saveDataPersonParameters = {
+		name: "me",
+		surname: "alwaysme"
+	};
 
-// Example POST call. AUTH = yes, HOOK = no, CACHE = overwrite
-CoreClient.callRESTFulPost(
-	'savePersonData',
-	saveDataPersonParameters,
-	errorCallback,
-	successCallback,
-	CACHE_OVERWRITE
-);
+	// Example POST call. AUTH = yes, HOOK = no, CACHE = overwrite
+	CoreClient.callRESTFulPost(
+		'savePersonData',
+		saveDataPersonParameters,
+		{
+			errorCallback: errorCallback,
+			successCallback: successCallback,
+			cache: CACHE_OVERWRITE
+		}
+	);
+});
